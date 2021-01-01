3bcc338603982da013f19c9b1e992f4284b637d8
markuspg <markuspg@users.noreply.github.com>
2021-01-01 15:39:20 +0100

    Encapsulate all accesses to Categories table

--- ibharchiver/database/tables/categories_table.py
+++ ibharchiver/database/tables/categories_table.py
@@ -7,3 +7,18 @@ class CategoriesTable:
         self.db_cursor = db_cursor
 
         self.db_cursor.execute(self.CREATE_TABLE_CMD)
+
+        self.cache = dict()
+        for category_row in self.db_cursor.execute('SELECT * FROM Categories;'):
+            self.cache[category_row[1]] = int(category_row[0])
+
+    def add_category(self, category):
+        if category not in self.cache:
+            self.db_cursor.execute('INSERT INTO Categories (cName) VALUES (?);', (category,))
+            self.cache[category] = self.get_category_id(category)
+
+    def get_category_id(self, category):
+        return int(self.db_cursor.execute('SELECT cId FROM Categories WHERE cName = ?;', (category,)).fetchone()[0])
+
+    def get_categories(self):
+        return self.cache.keys()
