e5424e82361c3e929e834b094382e8bd285120ba
markuspg <markuspg@users.noreply.github.com>
2021-01-01 15:30:56 +0100

    Encapsulate all accesses to Authors table

--- ibharchiver/database/tables/authors_table.py
+++ ibharchiver/database/tables/authors_table.py
@@ -7,3 +7,18 @@ class AuthorsTable:
         self.db_cursor = db_cursor
 
         self.db_cursor.execute(self.CREATE_TABLE_CMD)
+
+        self.cache = dict()
+        for author_row in self.db_cursor.execute('SELECT * FROM Authors;'):
+            self.cache[author_row[1]] = int(author_row[0])
+
+    def add_author(self, author):
+        if author not in self.cache:
+            self.db_cursor.execute('INSERT INTO Authors (aName) VALUES (?);', (author,))
+            self.cache[author] = self.get_author_id(author)
+
+    def get_author_id(self, author):
+        return int(self.db_cursor.execute('SELECT aId FROM Authors WHERE aName = ?;', (author,)).fetchone()[0])
+
+    def get_authors(self):
+        return self.cache.keys()
