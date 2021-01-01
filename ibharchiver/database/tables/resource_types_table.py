9ef06ba2eee90a45ead9653161f92272c80af270
markuspg <markuspg@users.noreply.github.com>
2021-01-01 15:53:37 +0100

    Encapsulate all accesses to ResourceTypes table

--- ibharchiver/database/tables/resource_types_table.py
+++ ibharchiver/database/tables/resource_types_table.py
@@ -12,3 +12,13 @@ class ResourceTypesTable:
         if not self.db_cursor.execute('SELECT * FROM ResourceTypes').fetchall():
             for resource_type in self.RESOURCE_TYPES:
                 self.db_cursor.execute('INSERT INTO ResourceTypes (rName) VALUES (?)', (resource_type,))
+
+        self.cache = dict()
+        for resource_type_row in self.db_cursor.execute('SELECT * FROM ResourceTypes;'):
+            self.cache[resource_type_row[1]] = int(resource_type_row[0])
+
+    def get_resource_type_id(self, resource_type):
+        return self.cache[resource_type]
+
+    def get_resource_types(self):
+        return self.cache.keys()
